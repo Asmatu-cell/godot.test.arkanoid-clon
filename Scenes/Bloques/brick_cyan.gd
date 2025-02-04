64959e00814040bf4b85f0d94342a90d81e6f2f0
Ferserk <fgmatgsmg@gmail.com>
2025-02-04 16:45:41 +0100

    FER - Cambios varios

---
+++ Scenes/Bloques/brick_cyan.gd
@@ -0,0 +1,14 @@
+extends Sprite2D
+
+@export var textura_spritesheet: Texture  # Asigna la imagen completa en el Inspector
+
+func _ready():
+	texture = textura_spritesheet  # Usa la imagen grande
+	set_estado(1)  # Para mostrar el segundo estado
+	set_estado(2)  # Para mostrar el tercer estado
+
+func set_estado(estado: int):
+	var ancho_sprite = 70  # Ajusta según el tamaño de cada imagen en el spritesheet
+	var alto_sprite = 30
+	region_enabled = true  # Activa el recorte de imagen
+	region_rect = Rect2(estado * ancho_sprite, 0, ancho_sprite, alto_sprite)
